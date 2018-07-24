f3e7bdcede6393fe0150b37e34abcb0adeab1e56
annab34 <17mcbean@gmail.com>
2018-07-24 14:20:02 +0000

    automatically backed up by learn

---
+++ methodspractice.rb
@@ -0,0 +1,14 @@
+my_name = 'Anna Friendshuh'
+birthdate = 'Aug13'
+birth_place = 'Cary'
+height = '70'
+
+puts my_name + ' was born in ' + ' and is from ' + 'and is'
+
+ puts 'What is your name?'
+ my_name = gets
+ puts 'What is your birthday?'
+ birthdate = gets 
+ puts 'where were you borne?'
+ birth_place = gets
+ puts ''
